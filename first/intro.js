c7119e8daca277a33d6b671ec4795c6759924898
ankit-chakraborty <ankitguitar117@gmail.com>
2024-01-18 01:34:29 +0530

    modifications on variable

--- first/intro.js
+++ first/intro.js
@@ -1,1 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("hello world in typescript");
+var greetings = "github";
+greetings = "git "; //this is possible whereas greetings=6 is not possible in typescript
+console.log(greetings);
--- first/intro.ts
+++ first/intro.ts
@@ -1,1 +1,9 @@
 console.log("hello world in typescript")
+
+let greetings: String = "github"
+
+greetings="git "  //this is possible whereas greetings=6 is not possible in typescript
+console.log(greetings)
+
+
+export {}
